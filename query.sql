14f12344ef12d134a639951666cc316a7a15547a
ds <ds@603-11>
2020-10-27 11:30:12 +0900

    query에 dummydata 추가

--- query.sql
+++ query.sql
@@ -89,3 +89,100 @@ CREATE TABLE DS_QA (
     
 CREATE SEQUENCE  "DB7"."DS_QA_SEQ"  MINVALUE 1 MAXVALUE 99999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL;
 /*</추가 10/26> ------------------------------------------------------------------------------------*/
+
+
+/*<추가 10/27> ------------------------------------------------------------------------------------*/
+
+
+create table ds_file(
+	  lecture_no NUMBER not null,
+      user_no NUMBER not null,
+      file_name varchar2(200) not null,
+      regdate date default sysdate,
+      constraint fk_ds_file foreign key(lecture_no) references ds_lecture(lecture_no) on delete cascade
+      );
+
+alter table ds_lecture add (file_status number(1) default 0 not null constraint booleantest_filestatus_CK check(file_status=0 or file_status=1));
+
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (4, '테스트강의실4', '테스트강의실설명4', 'childish1986');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (5, '테스트강의실5', '테스트강의실설명5', 'afbaseball1');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (6, '테스트강의실6', '테스트강의실설명6', 'AFLOL');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (7, '테스트강의실7', '테스트강의실설명7', 'childish1986');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (8, '테스트강의실8', '테스트강의실설명8', 'afbaseball1');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (9, '테스트강의실9', '테스트강의실설명9', 'AFLOL');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (10, '테스트강의실10', '테스트강의실설명10', 'childish1986');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (11, '테스트강의실11', '테스트강의실설명11', 'afbaseball1');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (12, '테스트강의실12', '테스트강의실설명12', 'AFLOL');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (13, '테스트강의실13', '테스트강의실설명13', 'childish1986');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (14, '테스트강의실14', '테스트강의실설명14', 'afbaseball1');
+insert into ds_lecture (lecture_no, lecture_name, lecture_description, lecture_afreecaid) values (15, '테스트강의실15', '테스트강의실설명15', 'AFLOL');
+
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (4, 'admin2', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '관리자2', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (5, 'admin3', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '관리자3', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (6, 'teacher2', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '강사2', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (7, 'teacher3', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '강사3', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (8, 'teacher4', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '강사4', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (9, 'student2', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생2', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (10, 'student3', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생3', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (11, 'student4', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생4', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (12, 'student5', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생5', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (13, 'student6', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생6', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (14, 'student7', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생7', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (15, 'student8', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생8', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (16, 'student9', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생9', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (17, 'student10', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생10', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (18, 'student11', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생11', '동성');
+insert into ds_user (user_no, user_id, user_pw, user_name, user_depart) values (19, 'student12', '$2a$10$SbAVSxN4302Gt6vCDtWOa.81ucLL7zzbBhjV7lRh0DEhm7pubjRr.', '학생12', '동성');
+
+insert into ds_auth values(4, 'ROLE_ADMIN');
+insert into ds_auth values(5, 'ROLE_ADMIN');
+insert into ds_auth values(6, 'ROLE_TEACHER');
+insert into ds_auth values(7, 'ROLE_TEACHER');
+insert into ds_auth values(8, 'ROLE_TEACHER');
+insert into ds_auth values(9, 'ROLE_STUDENT');
+insert into ds_auth values(10, 'ROLE_STUDENT');
+insert into ds_auth values(11, 'ROLE_STUDENT');
+insert into ds_auth values(12, 'ROLE_STUDENT');
+insert into ds_auth values(13, 'ROLE_STUDENT');
+insert into ds_auth values(14, 'ROLE_STUDENT');
+insert into ds_auth values(15, 'ROLE_STUDENT');
+insert into ds_auth values(16, 'ROLE_STUDENT');
+insert into ds_auth values(17, 'ROLE_STUDENT');
+insert into ds_auth values(18, 'ROLE_STUDENT');
+insert into ds_auth values(19, 'ROLE_STUDENT');
+
+insert into ds_class_list (lecture_no, user_no, class_status) values (4, 6, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (5, 6, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (6, 7, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (7, 2, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (8, 2, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (9, 2, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (10, 2, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (11, 2, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (12, 2, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (13, 2, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (14, 2, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (15, 2, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (1, 9, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (1, 10, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (1, 11, 'false');
+insert into ds_class_list (lecture_no, user_no, class_status) values (1, 12, 'false');
+insert into ds_class_list (lecture_no, user_no, class_status) values (4, 13, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (4, 14, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (4, 15, 'false');
+insert into ds_class_list (lecture_no, user_no, class_status) values (4, 16, 'false');
+
+insert into ds_class_list (lecture_no, user_no, class_status) values (4, 3, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (5, 3, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (6, 3, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (7, 3, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (8, 3, 'true');
+insert into ds_class_list (lecture_no, user_no, class_status) values (9, 3, 'false');
+insert into ds_class_list (lecture_no, user_no, class_status) values (10, 3, 'false');
+insert into ds_class_list (lecture_no, user_no, class_status) values (11, 3, 'false');
+insert into ds_class_list (lecture_no, user_no, class_status) values (12, 3, 'false');
+
+commit;
+
+/*<추가 10/27> ------------------------------------------------------------------------------------*/
+
